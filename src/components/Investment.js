c271a5f07cbdc7f2d796d5158944e33244aa5f24
calico-elivia <69293713+calico-elivia@users.noreply.github.com>
2020-10-15 19:19:46 +0800

    add investment.js banner section

---
+++ src/components/Investment.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Container, Row, Col, Button, Modal } from 'react-bootstrap'
+import $ from 'jquery'
+// import '../styles/ivestment.scss'yarn
+
+function Investment() {
+  return (
+    <>
+      <div className="bannerSec">
+        <h2 className="">Build Your Online Business with NEEDS</h2>
+        <Button className="btn btn-primary">立即開店 →</Button>
+      </div>
+    </>
+  )
+}
+
+export default Investment
