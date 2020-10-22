6e88e8ff0995dff0f2554647a27fd5ffb8dbd33c
calico-elivia <69293713+calico-elivia@users.noreply.github.com>
2020-10-22 16:24:12 +0800

    Correct App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,53 @@
 import React, { useState } from 'react'
 import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom'
 import './styles/global.scss'
+
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import Home from './pages/Home'
 import Investment from './pages/Investment'
+
+import BackEndSidebar from './components/backend/Sidebar'
+import BackEndNavbar from './components/backend/Navbar'
+import BackEndDashboard from './pages/BackEnd/Dashboard/Dashboard'
+
+const DynamicLayoutRoute = (props) => {
+  const { component: RoutedComponent, layout, ...rest } = props
+
+  const actualRouteComponent = (
+    <Route {...rest} render={(props) => <RoutedComponent {...props} />} />
+  )
+
+  switch (layout) {
+    case 'FRONT_END_NAV': {
+      return (
+        <>
+          <Navbar />
+          {actualRouteComponent}
+          <Footer />
+        </>
+      )
+    }
+    case 'BACK_END_NAV': {
+      return (
+        <>
+          <BackEndNavbar />
+          <BackEndSidebar />
+          {actualRouteComponent}
+        </>
+      )
+    }
+    default: {
+      return (
+        <>
+          <Navbar />
+          {actualRouteComponent}
+          <Footer />
+        </>
+      )
+    }
+  }
+}
 
 function App() {
   return (
@@ -13,12 +56,23 @@ function App() {
         <Navbar />
 
         <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/investment">
-            <Investment />
-          </Route>
+          <DynamicLayoutRoute
+            exact
+            path="/"
+            component={Home}
+            layout="FRONT_END_NAV"
+          />
+          <DynamicLayoutRoute
+            path="/Investment"
+            component={Investment}
+            layout="FRONT_END_NAV"
+          />
+
+          <DynamicLayoutRoute
+            path="/customer-backend"
+            component={BackEndDashboard}
+            layout="BACK_END_NAV"
+          />
         </Switch>
         <Footer />
       </>
