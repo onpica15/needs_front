36d530e79f9f27a3ae775cd26f16b387d860ad43
Winnie15951 <69294234+Winnie15951@users.noreply.github.com>
2020-10-23 10:02:17 +0800

    membersidebar
    membersidebar

---
+++ src/components/Memcard.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Memcard = () => {
+  return(
+
+      <>
+      
+
+          <div className="col-md-10 col-md-offset-2">
+            <div className="card">
+
+
+            </div>  
+          </div>
+      
+      
+      </> 
+    
+  )
+}
+
+export default Memcard
--- src/pages/Member/Member.js
+++ src/pages/Member/Member.js
@@ -1,8 +1,5 @@
 import React, { useState } from 'react'
-import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom'
-import './styles/global.scss'
-import Navbar from './components/Navbar'
-import Footer from './components/Footer'
+
 import MemberSidebar from './components/MemberSidebar'
 import Memcard from './pages/Memcard'
 import Investment from './pages/Investment'
