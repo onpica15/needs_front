626e85621d5e3681d48f1a0dbce0c394da631b7d
Elivia Chen <69293713+calico-elivia@users.noreply.github.com>
2020-10-26 01:50:59 +0800

    InvestmentPage - Spilt componets, css fixed, add animation to advantage section

--- src/pages/Investment/modules/Join.js
+++ src/pages/Investment/modules/Join.js
@@ -1,28 +1,30 @@
-import React, { useState, useEffect } from 'react'
+import React, { useEffect } from 'react'
 import { Button } from 'react-bootstrap'
 import { useTransition, animated } from 'react-spring'
 
-const Join = ({ show }) => {
+const Join = ({ show, aniRef }) => {
   useEffect(() => {}, [show])
   const transitions = useTransition(show, null, {
-    config: { duration: 1000 },
-    from: { transform: 'translateY(100px)', opacity: 0 },
+    config: { duration: 1800 },
+    from: { transform: 'translateY(150px)', opacity: 0 },
     enter: { transform: 'translateY(0px)', opacity: 1 },
   })
-  return transitions.map(
-    ({ item, key, props }) =>
-      item && (
-        <div className="joinSec" key={key}>
-          <animated.div key={key} style={props} className="joinContent">
-            <span>立即申請加入 NEEDS！</span>
-            <span>
-              馬上開始填寫申請表，加入這些優秀設計品牌的行列，和
-              NEEDS一起為你的品牌開啓新的歷程吧！
-            </span>
-            <Button variant="light">立即開店 →</Button>
-          </animated.div>
-        </div>
-      )
+  return (
+    <div className="joinSec" ref={aniRef}>
+      {transitions.map(
+        ({ item, key, props }) =>
+          item && (
+            <animated.div key={key} style={props} className="joinContent">
+              <span>立即申請加入 NEEDS！</span>
+              <span>
+                馬上開始填寫申請表，加入這些優秀設計品牌的行列，和
+                NEEDS一起為你的品牌開啓新的歷程吧！
+              </span>
+              <Button variant="light">立即開店 →</Button>
+            </animated.div>
+          )
+      )}
+    </div>
   )
 }
 
