5b17945e66f3799a9f39cb668baaf22bf6dae869
LibellulaYang <59201407+LibellulaYang@users.noreply.github.com>
2020-10-25 19:23:26 +0800

    recommend section adjust

--- src/pages/BackEnd/TemplateList/TemplateList.js
+++ src/pages/BackEnd/TemplateList/TemplateList.js
@@ -9,26 +9,50 @@ import {
   Container,
   Card,
   CardDeck,
-  Row,
 } from 'react-bootstrap'
 function TemplateList(props) {
   
   return (
     <>
+    <div className="template">
+        <Col className="offset-2 xs={10}">
+            <Container fluid className="main-bg d-flex justify-content-end">
+                <div className="main-div rounded ">
+                  <p>熱門程度</p>
+                </div>
+                <div className="main-div rounded">
+                <p>pro/free</p>
+                </div>
+                <hr/>
+            </Container>
+        </Col>
+    </div>
+
+
+    {/* ############# */}
 
       <div className="template">
         <Col className="main offset-2" xs={10}>
             <Container fluid className="main-bg">
-              <div className="recommend-main rounded mh-100 pb-2 d-flex align-items-center ">
-                <div className="pt-5 pb-2 d-flex fa-align-center align-item">
-                  <img src={TemplatepicBig} class="img-fluid" alt="Responsive image"></img>
-                  <div className="d-flex flex-column align-items-center justify-content-between"> 
-                    <h4>Narrative</h4>
-                    <h6>方案：FREE</h6>
-                    <button className="btn-gray">加入願望清單</button>
-                    <button className="btn-purple">立即套用</button>
-                  </div>
+
+              <div className="main-div recommend rounded d-flex align-items-center ">
+
+              <div className="row rounded d-flex align-items-center">
+                <div className="col-8">
+                <img src={Templatepic1} class="main-small-pic" alt="Responsive image"></img>
+                <img src={TemplatepicBig} className="img-fluid main-bg-pic" alt="Responsive image"></img>
                 </div>
+                <div className="col-4 flex-column justify-content-end">
+                    <h1 >Narrative</h1>
+                    <h6 >方案：FREE</h6>
+                    <div className="pb-2">
+                    <button className="btn-bg gray">加入願望清單</button>
+                    </div>
+                    <div>
+                    <button className="btn-bg purple">立即套用</button>
+                    </div>
+                </div>
+            </div>
               </div>
             </Container>
             </Col>
@@ -47,9 +71,9 @@ function TemplateList(props) {
                   <Card.Text className="text-center">
                     FREE
                   </Card.Text>
-                  <Container fluid className="d-flex mw-100">
-                  <button className="btn btn-secondary float-left">加入收藏</button>
-                  <button className="btn btn-primary">立即套用</button>
+                  <Container className="d-flex p-0">
+                  <button className="btn-bg gray left">加入收藏</button>
+                  <button className="btn-bg purple right">立即套用</button>
                   </Container>
                 </div>
               </Card>
@@ -61,9 +85,9 @@ function TemplateList(props) {
                   <Card.Text className="text-center">
                     FREE
                   </Card.Text>
-                  <Container fluid className="d-flex mw-100">
-                  <button className="btn btn-secondary float-left">加入收藏</button>
-                  <button className="btn btn-primary">立即套用</button>
+                  <Container className="d-flex p-0">
+                  <button className="btn-bg gray left">加入收藏</button>
+                  <button className="btn-bg purple right">立即套用</button>
                   </Container>
                 </div>
               </Card>
@@ -75,9 +99,9 @@ function TemplateList(props) {
                   <Card.Text className="text-center">
                     FREE
                   </Card.Text>
-                  <Container fluid className="d-flex mw-100">
-                  <button className="btn btn-secondary float-left">加入收藏</button>
-                  <button className="btn btn-primary">立即套用</button>
+                  <Container className="d-flex p-0">
+                  <button className="btn-bg gray left">加入收藏</button>
+                  <button className="btn-bg purple right">立即套用</button>
                   </Container>
                 </div>
               </Card>
