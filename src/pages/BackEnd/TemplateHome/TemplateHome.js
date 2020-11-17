6ea1a4e664dc31bc9327273df039bcfe3aea2d1a
LibellulaYang <59201407+LibellulaYang@users.noreply.github.com>
2020-11-17 14:00:24 +0800

    add breadcrumb

--- src/pages/BackEnd/TemplateHome/TemplateHome.js
+++ src/pages/BackEnd/TemplateHome/TemplateHome.js
@@ -6,7 +6,7 @@ import swal from 'sweetalert';
 import { Col, Container, Card, CardDeck, Row , Modal ,Button } from 'react-bootstrap'
 
 import TemplatepicBig from './images/minimal_pro.png'
-import './Styles/TemplateHome.scss'
+import './styles/TemplateHome.scss'
 import '../../../styles/Backend/_backend.scss'
 
 import { MdVisibility, MdVisibilityOff, MdEdit } from 'react-icons/md'
@@ -99,7 +99,7 @@ const updateFavToLocalStorage = function (item) {
         <Col className="offset-2" xs={10}>
           <div className="container-fluid w-80">
             <h4 className="mt-4">主題</h4>
-            <Button className="mt-4 btn-light" onClick={()=>{props.history.push('/homepage/Pommedepin111')}}>
+            <Button className="mt-4 btn-light" onClick={()=>{props.history.push('/homepage/Pommedepin111/12')}}>
               <MdVisibility /> 查看目前主頁
             </Button>
             <hr />
