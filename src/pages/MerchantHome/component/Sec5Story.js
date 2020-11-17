de49b2070da755c8b40d63665ad2d8330323e4e1
LibellulaYang <59201407+LibellulaYang@users.noreply.github.com>
2020-11-17 13:51:11 +0800

    css fixed

--- src/pages/MerchantHome/component/Sec5Story.js
+++ src/pages/MerchantHome/component/Sec5Story.js
@@ -15,13 +15,9 @@ function Sec5Story(props){
             <h2>品牌故事</h2>
             </div>
             </div>
-            <ScrollParallax 
-                            animation={{ x: 0, opacity: 1, playScale: [0.1, 0.8] }}
-                            style={{ transform: 'translateX(100px)', opacity: 0 }}
-                            always={true}
-                            duration={1000}
-                            > 
+            
             <div className="d-flex adjust">
+           
             {/* <div className="brandStory_img" style={{ backgroundImage : `url(${displayStoryImg})`}}></div> */}
             {merchantInfos.brandStory_img ? (
               <img
@@ -32,13 +28,23 @@ function Sec5Story(props){
             ) : (
               ''
             )}
+         
             {/* http://localhost:5000/BrandStoryImg/ */}
+            <ScrollParallax 
+                            animation={{ x: 0, opacity: 1, playScale: [0.1, 0.8] }}
+                            style={{ transform: 'translateX(100px)', opacity: 0 }}
+                            always={false}
+                            duration={1000}
+                            > 
             <div 
                 className=" col-6 text-wrapper ml-5"
                 dangerouslySetInnerHTML={{ __html: merchantInfos.brand_story}}
-                ></div>
+                >
+            
             </div>
             </ScrollParallax>
+            </div>
+     
             </div>
             )
                 )}  
